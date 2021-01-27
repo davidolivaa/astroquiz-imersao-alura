2bd10f555db4f4ae8608c9e6bfb9b844dd73d098
David Oliveira Silva <davidoliva96@gmail.com>
2021-01-27 00:38:38 -0300

    Commit initial

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import db from '../'
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.secondary};
+`
+
+
+export default function Home() {
+  return (
+    <div style={{backgroundImage: ''}}>
+
+    </div>
+  )
+  
+}
